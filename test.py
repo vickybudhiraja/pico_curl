74715c37fcefdf00deb9e30533de55ca4e6c5859
vicky <vicky.vigia@gmail.com>
2025-03-14 10:17:21 +0530

    feat: micropython setup

---
+++ test.py
@@ -0,0 +1,24 @@
+MPY: soft reboot
+MicroPython v1.24.1 on 2024-11-29; Raspberry Pi Pico W with RP2040
+Type "help()" for more information.
+>>> import machine
+>>> led = machine.Pin("LED", machine.Pin.OUT)
+>>> led.off()
+>>> led.on()
+>>> led.off()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
+>>> led.toggle()
